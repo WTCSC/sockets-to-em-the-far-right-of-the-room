de2cb4d3b3f0f15f6c50d5f894acb0a12b1a169c
C-Dingus <rickardc@CSGP.EDU>
2026-02-27 10:10:04 -0700

    they're going to take my house

--- server.py
+++ server.py
@@ -7,29 +7,64 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind to 0.0.0.0:5000
 server.bind(("0.0.0.0", 5000))
 
-#amount of clients currently connected to the server
+#server sends out prompt to all clients and they must catch it
+#clients send in their cards
+#server sends out everyones played cards to everyone
+#clients send in votes 
+#server returns winner
+#repeat
 clients_num = 0
-start_votes = 0
+start = False
 
+prompt = "_"
+
+ans = []
 
 def parse_msg(client, name, message):
+    global start
     words = message.split()
-    print("ok cool")
+    match words[0]:
+        case "play":
+            ans.append(message.split("play")[1])
+        case "draw":
+            #draw a card and send it to the player
+            client.send()
+        case "vote":
+            #vote on card
+            client.send()
+        case "prompt":
+            client.send(prompt.encode())
+        case "start":
+            #start the game
+            print("start")
+            start = True
+            #client.send("recieved".encode())
+        case _:
+            client.send(f"unknown message starter {words[0]}".encode())
+            print(f"recived unknown message from client {name}: {message}")
+
 
 #read client inputs
 def read_client(client, name):
+    global clients_num
     while True:
         msg = client.recv(1024).decode()
         if not msg:
             break
         parse_msg(client, name, msg)
+        client.send("ok".encode())
     #reduce clients when someone leaves
-    client.close()
+    #clients_num -= 1
+    print(f"client {name} left")
     clients_num -= 1
+    client.close()
+    
 
 clients = []
 
-while start_votes < clients_num or clients_num < 2:
+
+while not start:
+    print(start)
     # Listen for connections
     server.listen(1)
     print("Waiting for connection...")
@@ -38,11 +73,29 @@ while start_votes < clients_num or clients_num < 2:
     client, addr = server.accept()
     print(f"Connected to {addr}")
 
+
     clients_num += 1
     # Receive and echo messages
     t = threading.Thread(target=read_client, args=(client, clients_num, ))
     t.start()
     clients.append(client)
 
+#tell clients to start
+for client in clients:
+    client.send("start".encode())
+
+#get prompt 
+prompt = "ok"
+
+while len(ans) < clients_num:
+    _ = 0
+
+pack = ""
+
+for answer in ans:
+    pack.append(f"\n\n{answer}")
+
+for client in clients:
+    client.send(pack.encode())
 
 server.close()
