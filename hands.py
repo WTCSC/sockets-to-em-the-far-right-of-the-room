83c475b3a9d417c98920dba2c85b1ac8437319e0
Blanchard <blanchardd@CSGP.EDU>
2026-03-05 10:17:32 -0700

    Small changes

--- hands.py
+++ hands.py
@@ -36,3 +36,20 @@ def pick_card(hand, num_of_cards = 5):
 
     print(f"You picked card {answer}: \"{hand_string[int(answer) - 1]}\"")
     return hand.split('.')[int(answer) - 1]
+
+def get_prompt(banned):
+    if len(banned) > (71 - num_of_players):
+        del banned[:50]
+
+    for player_num in range (0, num_of_players):
+        hand = hands[player_num]
+
+        while len(hand) < 5:
+            new_card = randint(0, 71)
+            if new_card not in banned:
+                hand.append(new_card)
+                banned.append(new_card)
+
+        hands[player_num] = hand
+
+    return hands, banned
